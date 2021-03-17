2f0819090fc684b41253a64b09d41de485a92d3f
Sebastian <sebastian.presser@gmail.com>
2021-03-17 19:55:11 +0100

    linux support and possibility to quit threads over commandline

--- ipcam-tool.py
+++ ipcam-tool.py
@@ -1,18 +1,12 @@
-# pip install opencv-python
-import cv2
-import cv2 as cv
-# pip install numpy==1.19.3
-import numpy as np
 import os
-from datetime import *
-import requests
-from threading import Thread
-
-debug = False
-
 #################################################################
+nogui = False
 
-basepath = 'H:\\cam\\'
+if os.name == 'nt':
+    basepath = 'H:\\cam\\'
+else:
+    basepath = '/media/hdd/cams/'
+    nogui = True
 
 logfile = basepath + 'log.txt'
 
@@ -20,6 +14,7 @@ win_w = 768
 win_h = 432 # 1080p / 2.5
 
 fps_default = 25.0
+
 
 # an array of 8 key-value pairs for each camera instance
 # (string) name:                anything to identify the camera (affects folder and file names)
@@ -30,6 +25,7 @@ fps_default = 25.0
 # (int)    timelapse_speed:     only record every nth frame (timelapse records at 60fps)
 # (bool)   show_overlay:        if motion detection is shown in the window
 # (bool)   update_screen:       if the desktop window is updated
+
 devices = [
     {
         "enabled": True,
@@ -37,10 +33,10 @@ devices = [
         "url": "rtsp://admin:password@192.168.2.10:554/live/av0", 
         "record_motion": False, 
         "motion_sensitivity": 100, 
-        "record_timelapse": False, 
-        "timelapse_speed": 16, 
+        "record_timelapse": True, 
+        "timelapse_speed": 256, 
         "show_overlay": False, 
-        "update_screen": True
+        "update_screen": False
     },
     {
         "enabled": True,
@@ -48,27 +44,135 @@ devices = [
         "url": "rtsp://admin:password@192.168.2.11:554/live/av0", 
         "record_motion": False, 
         "motion_sensitivity": 100, 
-        "record_timelapse": False, 
-        "timelapse_speed": 16, 
+        "record_timelapse": True, 
+        "timelapse_speed": 256, 
         "show_overlay": False, 
-        "update_screen": True
+        "update_screen": False
     },
 ]
 
 #################################################################
 
+debug = False
+
+# pip install opencv-python
+import cv2
+#import cv2 as cv
+# pip install numpy==1.19.3
+import numpy as np
+import os
+from datetime import *
+import requests
+from threading import Thread
+import signal
+import sys
+
+
+# Windows
+if os.name == 'nt':
+    import msvcrt
+
+# Posix (Linux, OS X)
+else:
+    import sys
+    import termios
+    import atexit
+    from select import select
+
+
+class KBHit:
+
+    def __init__(self):
+        '''Creates a KBHit object that you can call to do various keyboard things.
+        '''
+
+        if os.name == 'nt':
+            pass
+
+        else:
+
+            # Save the terminal settings
+            self.fd = sys.stdin.fileno()
+            self.new_term = termios.tcgetattr(self.fd)
+            self.old_term = termios.tcgetattr(self.fd)
+
+            # New terminal setting unbuffered
+            self.new_term[3] = (self.new_term[3] & ~termios.ICANON & ~termios.ECHO)
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.new_term)
+
+            # Support normal-terminal reset at exit
+            atexit.register(self.set_normal_term)
+
+
+    def set_normal_term(self):
+        ''' Resets to normal terminal.  On Windows this is a no-op.
+        '''
+
+        if os.name == 'nt':
+            pass
+
+        else:
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.old_term)
+
+
+    def getch(self):
+        ''' Returns a keyboard character after kbhit() has been called.
+            Should not be called in the same program as getarrow().
+        '''
+
+        s = ''
+
+        if os.name == 'nt':
+            return msvcrt.getch().decode('utf-8')
+
+        else:
+            return sys.stdin.read(1)
+
+
+    def getarrow(self):
+        ''' Returns an arrow-key code after kbhit() has been called. Codes are
+        0 : up
+        1 : right
+        2 : down
+        3 : left
+        Should not be called in the same program as getch().
+        '''
+
+        if os.name == 'nt':
+            msvcrt.getch() # skip 0xE0
+            c = msvcrt.getch()
+            vals = [72, 77, 80, 75]
+
+        else:
+            c = sys.stdin.read(3)[2]
+            vals = [65, 67, 66, 68]
+
+        return vals.index(ord(c.decode('utf-8')))
+
+
+    def kbhit(self):
+        ''' Returns True if keyboard character was hit, False otherwise.
+        '''
+        if os.name == 'nt':
+            return msvcrt.kbhit()
+
+        else:
+            dr,dw,de = select([sys.stdin], [], [], 0)
+            return dr != []
+
+
 def display_help():
-    print("##########################################")
-    print("# h = display this help text             #")
-    print("# r = toggle record motion               #")
-    print("# o = draw motion detection overlay      #")
-    print("# u = update frame window                #")
-    print("# t = toggle record timelapse            #")
-    print("# + = increase timelapse speed           #")
-    print("# - = decrease timelapse speed           #")
-    print("# c = print current cam configuration    #")
-    print("# q = quit application                   #")
-    print("##########################################")
+    print("#################################################")
+    print("# h = display this help text                    #")
+    print("# r = toggle record motion                      #")
+    print("# o = draw motion detection overlay             #")
+    print("# u = update frame window                       #")
+    print("# t = toggle record timelapse                   #")
+    print("# + = increase timelapse speed                  #")
+    print("# - = decrease timelapse speed                  #")
+    print("# c = print current cam configuration           #")
+    print("# q = quit thread (works in console, too)       #")
+    print("#################################################")
 
 # log timestamp, threadname and any string to file and display on screen
 def log(name, logstr):
@@ -79,19 +183,45 @@ def log(name, logstr):
     with open(logfile, "a") as myfile:
         myfile.write(msg + "\n")
 
-def get_file_path(name, record_timelapse):
+def get_file_path(devname, record_timelapse):
     global basepath
     timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    pathstring = basepath + name + "\\" + timestamp
+    
+    videopath = ""
+    timelapsepath = ""
+    
+    if os.name == 'nt':
+        videopath = basepath + devname + "\\"
+        timelapsepath = videopath + "timelapse\\"
+    else:
+        videopath = basepath + devname + "/"
+        timelapsepath = videopath + "timelapse/"
+    
+    # check if there's a directory for this camera already
+    if not os.path.isdir(videopath):
+        os.mkdir(videopath)
+        log(devname, "video path didn't exist and was created")
+    
+    if not os.path.isdir(timelapsepath):
+        os.mkdir(timelapsepath)
+        log(devname, "timelapse path didn't exist and was created")
+    
+    pathstring = ""
+    
     if record_timelapse:
-        pathstring = pathstring + "-timelapse"
+        pathstring = timelapsepath + timestamp + "-timelapse"
+    else:
+        pathstring = videopath + timestamp
+        
     return pathstring + ".avi"
-
 
 log("application", "started")
 log("application", "log file: " + logfile)
+log("application", "video base path: " + basepath)
 
 display_help()
+
+kb = KBHit()
 
 # set up video writer
 # for details see https://docs.opencv.org/master/dd/d43/tutorial_py_video_display.html#nav-path:~:text=Saving%20a%20Video
@@ -99,7 +229,7 @@ fourcc = cv2.VideoWriter_fourcc(*'DIVX')
 
 def capture_device_thread(dev, index):
     log(dev["name"], "started thread " + str(index))
-    global fourcc, basepath, win_w, win_h, fps_default
+    global fourcc, basepath, win_w, win_h, fps_default, nogui
 
     exit_thread = False
     update_screen = dev["update_screen"]
@@ -108,20 +238,18 @@ def capture_device_thread(dev, index):
     record_timelapse = dev["record_timelapse"]
     timelapse_speed = dev["timelapse_speed"]>>1
     show_overlay = dev["show_overlay"]
-    videopath = basepath + dev["name"] + "\\"
     windowname = "[" + dev["name"] + "] ipcam tool"
     fps = fps_default
-    
+        
     log(dev["name"], ("settings:" + 
-        " update_screen=" + str(update_screen) + 
-        " record_motion=" + str(record_motion) + 
-        " motion_sensitivity=" + str(motion_sensitivity) + 
-        " record_timelapse=" + str(record_timelapse) + 
-        " timelapse_speed=" + str(timelapse_speed) + 
-        " show_overlay=" + str(show_overlay) + 
-        " videopath=" + videopath + 
-        " windowname=" + windowname + 
-        " fps=" + str(fps))
+        "\n update_screen=" + str(update_screen) + 
+        "\n record_motion=" + str(record_motion) + 
+        "\n motion_sensitivity=" + str(motion_sensitivity) + 
+        "\n record_timelapse=" + str(record_timelapse) + 
+        "\n timelapse_speed=" + str(timelapse_speed) + 
+        "\n show_overlay=" + str(show_overlay) + 
+        "\n windowname=" + windowname + 
+        "\n fps=" + str(fps))
     )
     
     
@@ -132,17 +260,6 @@ def capture_device_thread(dev, index):
         show_overlay = True
         windowname = windowname + " (debug)"
     
-    log(dev["name"], "video path: " + videopath)
-    
-    # check if there's a directory for this camera already
-    if not os.path.isdir(basepath + dev["name"]):
-        os.mkdir(basepath + dev["name"])
-        log(dev["name"], "video path didn't exist and was created")
-    
-    if not os.path.isdir(basepath + dev["name"] + "\\timelapse"):
-        os.mkdir(basepath + dev["name"] + "\\timelapse")
-        log(dev["name"], "timelapse path didn't exist and was created")
-        
     out = None
     out_timelapse = None
     baseline_image = None
@@ -152,19 +269,24 @@ def capture_device_thread(dev, index):
     framecounter = 0
 
     while not exit_thread:
-        if cv2.getWindowProperty('window-name', 0) == -1:
-            cv2.namedWindow(windowname, cv2.WINDOW_NORMAL)
-            
-            cv2.resizeWindow(windowname, win_w, win_h)
-            
-            # get correct window position
-            win_x = int(((index//4)*2 + index%2) * win_w)
-            win_y = int(((index//2) % 2) * win_h * 1.07)
-            
-            cv2.moveWindow(windowname, win_x, win_y)
-            
-            log(dev["name"], "preview window opened at " + str(win_x) + "/" + str(win_y))
-        
+        if not nogui:
+            # on windows create window and stuff
+            if cv2.getWindowProperty(windowname, 0) == -1:
+                cv2.namedWindow(windowname, cv2.WINDOW_NORMAL)
+                
+                cv2.resizeWindow(windowname, win_w, win_h)
+                
+                # get correct window position
+                win_x = int(((index//4)*2 + index%2) * win_w)
+                win_y = int(((index//2) % 2) * win_h * 1.07)
+                
+                cv2.moveWindow(windowname, win_x, win_y)
+                
+                log(dev["name"], "preview window opened at " + str(win_x) + "/" + str(win_y))
+        else:
+            # my linux machines don't have a gui
+            update_screen = False
+
         cap = cv2.VideoCapture(dev["url"])
         
         width = cap.get(cv2.CAP_PROP_FRAME_WIDTH) # float
@@ -177,6 +299,16 @@ def capture_device_thread(dev, index):
         frame_with_motion = False
         
         while(cap.isOpened()):
+
+            # check for esc key in console
+            if kb.kbhit():
+                c = kb.getch()
+                #print(ord(c))
+                if ord(c) == 113: # q
+                    log(dev["name"], "shutting down thread from console")
+                    exit_thread = True
+                    break
+                
             ret, frame = cap.read()
             old_frame_with_motion = frame_with_motion
             frame_with_motion = False
@@ -190,9 +322,8 @@ def capture_device_thread(dev, index):
                 
                 # record timelapse frames
                 if record_timelapse and ((framecounter % timelapse_speed) == 0):
-                    #print(framecounter)
                     if not out_timelapse:
-                        file_timelapse = get_file_path(dev["name"] + "\\timelapse", True)
+                        file_timelapse = get_file_path(dev["name"], True)
                         if os.path.isfile(file_timelapse):
                             os.remove(file_timelapse)
                         out_timelapse = cv2.VideoWriter(file_timelapse, fourcc, 60.0, (int(width),int(height)))
@@ -355,8 +486,10 @@ def capture_device_thread(dev, index):
         log(dev["name"], "motion recording ended")
 
     #cv2.destroyAllWindows()
-    
-    cv2.destroyWindow(windowname)
+    if not nogui:
+        if cv2.getWindowProperty(windowname, 0) == -1:
+            cv2.destroyWindow(windowname)
+
     log(dev["name"], "thread exited")
 
 index = 0
